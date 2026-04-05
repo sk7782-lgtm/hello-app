6a4e0ce611f7f0042618a0c095f12d851f19978b
hyndavi <sk7782@srmist.edu.in>
2026-04-05 18:07:46 +0530

    UC6: Substring to remove trailing delimiter

--- HelloApp.java
+++ HelloApp.java
@@ -4,9 +4,16 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            String result = "Hello ";
+
             for (String name : args) {
-                System.out.println("Hello " + name);
+                result += name + ", ";
             }
+
+            // Remove last comma and space
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println(result);
         }
     }
 }
