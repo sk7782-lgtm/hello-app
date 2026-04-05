64ebcbce05b2a50c42d6657e7a1fd4d052eae568
hyndavi <sk7782@srmist.edu.in>
2026-04-05 16:23:32 +0530

    UC3: Display name with defalut

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,16 @@
 
 
 
-
 public class HelloApp {
     public static void main(String[] args) {
+        String name;
 
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
+            name = args[0];
         } else {
-            System.out.println("Hello User");
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
